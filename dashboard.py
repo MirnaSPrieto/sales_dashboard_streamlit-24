4742cc1b5d25a526141210d2371c2d2394741fa6
MirnaSPrieto <134005874+MirnaSPrieto@users.noreply.github.com>
2024-07-20 03:26:44 -0300

    cambio de path

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,6 @@ import pandas as pd
 st.title('Dashboard de Ventas :shopping_trolley:') 
 
 #Enlacenlace publico a B Ventas: https://github.com/MirnaSPrieto/sales_dashboard_streamlit-24/blob/main/base_ventas.csv
-df_ventas = pd.read_csv('https://raw.githubusercontent.com/MirnaSPrieto/sales_dashboard_streamlit-24/blob/main/base_ventas.csv')
+df_ventas = pd.read_csv('base_ventas.csv')
 # Mostrar los datos en un dataframe de Streamlit
 st.dataframe(df_ventas)
