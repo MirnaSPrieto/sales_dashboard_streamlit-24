b82a48f9a1729ec3094675433f0417713ad2fe36
MirnaSPrieto <134005874+MirnaSPrieto@users.noreply.github.com>
2024-07-20 03:23:42 -0300

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,9 @@
-pip install streamlit 
 import streamlit as st
 import pandas as pd
 
 st.title('Dashboard de Ventas :shopping_trolley:') 
 
-df_ventas = pd.read_cvs ('base_ventas.csv')
-
+#Enlacenlace publico a B Ventas: https://github.com/MirnaSPrieto/sales_dashboard_streamlit-24/blob/main/base_ventas.csv
+df_ventas = pd.read_csv('https://raw.githubusercontent.com/MirnaSPrieto/sales_dashboard_streamlit-24/blob/main/base_ventas.csv')
+# Mostrar los datos en un dataframe de Streamlit
 st.dataframe(df_ventas)
